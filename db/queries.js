5ba46fb3c83112b60c2cd6b044d82b3cffe79f02
nepallium <122187419+nepallium@users.noreply.github.com>
2026-04-15 14:23:13 -0400

    add single product info getter

--- db/queries.js
+++ db/queries.js
@@ -2,18 +2,34 @@ import pool from "./pool.js";
 // for deletion, learn how to reverse delete query to test deleting whole categories
 // deleting category should not delete the instrument
 
+const joinAll = 
+    `FROM instruments i
+    LEFT JOIN instruments_categories i_c ON i_c.instrument_id = i.id
+    LEFT JOIN categories c ON c.id = i_c.category_id
+    LEFT JOIN brands b ON b.id = i.brand_id`
+
+const joinAndSelectAll = 
+    `
+    SELECT i.id, i.instrument_name, b.brand_name,
+    COALESCE(json_agg(c.category_name) FILTER (WHERE c.category_name IS NOT NULL), '[]') AS categories
+    ${joinAll}
+    `
+
 export async function getAllProducts() {
   const { rows } = await pool.query(`
-    SELECT i.id, i.instrument_name, b.brand_name,
-      COALESCE(json_agg(c.category_name) FILTER (WHERE c.category_name IS NOT NULL), '[]') AS categories
-    FROM instruments i
-    LEFT JOIN instrument_categories i_c ON i_c.instrument_id = i.id
-    LEFT JOIN categories c ON c.id = i_c.category_id
-    LEFT JOIN brands b ON b.id = i.brand_id
+    ${joinAndSelectAll}
     GROUP BY i.id, b.brand_name
     `);
 
   return rows;
+}
+
+export async function getProductInfo(productId) {
+  const {rows} = await pool.query(`
+    ${joinAndSelectAll}
+    WHERE id = $1
+    GROUP BY i.id, b.brand_name
+    `, [productId])
 }
 
 export async function deleteProduct(productId) {
@@ -54,11 +70,7 @@ export async function getAllCategories() {
 
 export async function getProductsInCategory(category) {
   const {rows} = await pool.query(`
-    SELECT i.id, i.name, b.brand_name
-    FROM instruments i
-    LEFT JOIN instruments_categories i_c ON i_c.instrument_id = i.id
-    LEFT JOIN categories c ON c.id = i_c.category_id
-    LEFT JOIN brands b ON b.id = i.brand_id
+    ${joinAndSelectAll}
     WHERE c.name = $1
     GROUP BY i.id, b.brand_name
     `, [category])
