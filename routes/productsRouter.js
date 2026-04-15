5aa7795e37d1c13c67c39f34d2132b533087a6ee
nepallium <122187419+nepallium@users.noreply.github.com>
2026-04-15 14:48:06 -0400

    add incomplete update and create product

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,6 +4,8 @@ import * as controller from "../controllers/productController.js";
 const products = Router();
 
 products.get("/", controller.showAllProducts);
+products.get("/update/:id", controller.showUpdateProduct)
+
 products.post("/delete/:id", controller.deleteProduct)
 products.post("/update/:id", controller.updateProduct)
 
